0dd772ad577bf5ec86f29a63ec8b6daa740dddd6
avi <hi@avi.im>
2015-07-10 20:07:05 +0530

    made the import work fine in both examples. (fixes #2)

--- prawoauth2/__init__.py
+++ prawoauth2/__init__.py
@@ -1,2 +1,4 @@
-from PrawOAuth2Mini import PrawOAuth2Mini  # noqa
-from PrawOAuth2Server import PrawOAuth2Server  # noqa
+from __future__ import absolute_import
+
+from .PrawOAuth2Mini import PrawOAuth2Mini  # noqa
+from .PrawOAuth2Server import PrawOAuth2Server  # noqa
